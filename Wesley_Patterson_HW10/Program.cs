26e2227e011b0f361acb16f8e5f05feafcaa2327
Wesley Patterson <Wesley.D.Patterson-1@ou.edu>
2023-05-01 16:18:47 -0500

    Add project files.

---
+++ Wesley_Patterson_HW10/Program.cs
@@ -0,0 +1,104 @@
+﻿// HW10
+// Wesley Patterson
+// 113562579
+using a;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+
+
+int cogNum = 0;
+int gearNum = 0;
+
+List<Reciept> rList = new List<Reciept>();
+
+Console.Write("Do you want to print a new receipt? (y/n)");
+string userInput = Console.ReadLine().ToLower();
+
+while (userInput == "y")
+{
+    Console.Write("Please input the number of cogs: ");
+    cogNum = Convert.ToInt16(Console.ReadLine());
+
+    Console.Write("Please input the number of gears: ");
+    gearNum = Convert.ToInt16(Console.ReadLine());
+
+    Console.Write("Please input the Customer ID:");
+    int custID = Convert.ToInt16(Console.ReadLine());
+
+
+    //Reciept r = new Reciept();
+    //r.customerID = custID;
+    //r.cogQuant = cogNum;
+    //r.gearQuant = gearNum;
+
+    //same as 
+
+    Reciept r = new Reciept(custID, cogNum, gearNum);
+
+    r.PrintReciept();
+
+    rList.Add(r);
+    Console.Write("Do you want to enter a new receipt? (y/n)");
+    userInput = Console.ReadLine().ToLower();
+}
+do
+{
+    Console.WriteLine("Please choose from the options:");
+    Console.WriteLine("1: Print all receipt of one customer");
+    Console.WriteLine("2: Print all receipt for today");
+    Console.WriteLine("3: Print the highest total receipt");
+    Console.WriteLine("4.Print the average grand total");
+    Console.WriteLine("Press other keys to end");
+
+    userInput = Console.ReadLine();
+
+    if(userInput == "1")
+    {
+         
+        Console.WriteLine("Please enter a cust ID:");
+        int idStr = Convert.ToInt16(Console.ReadLine());
+        for(int i = 1; i<=rList.Count;i++)
+        {
+            if(rList[i-1].customerID == idStr)
+            {
+                rList[i-1].PrintReciept();
+            }
+        }
+
+    } else if(userInput == "2")
+    {
+        for (int i = 0; i < rList.Count; i++)
+        {
+                rList[i].PrintReciept();
+        }
+    } else if(userInput == "3")
+    {
+        Reciept highest = rList[0];
+        for(int i = 1; i<=rList.Count; i++)
+        {
+            if (rList[i-1].CalculateTotal() > highest.CalculateTotal())
+            {
+                highest = rList[i - 1];
+            }
+        }
+        Console.WriteLine("receipt with highest total: ");
+        highest.PrintReciept();
+
+    } else if(userInput == "4" )
+    {
+        double sumTotal = 0;
+        for(int i = 0; i<rList.Count; i++)
+        {
+            sumTotal = sumTotal + rList[i].CalculateTotal();
+        }
+        double averageTotal = sumTotal/ rList.Count;
+        Console.WriteLine($"The average grandTotal is: {averageTotal:C2}");
+    } else
+    {
+        break;
+    }
+
+} while (userInput == "1" || userInput == "2" || userInput == "3" || userInput =="4");
+
+
---
+++ Wesley_Patterson_HW10/Reciept.cs
@@ -0,0 +1,75 @@
+﻿
+namespace a
+{
+    public class Reciept
+    {
+        public int customerID { get; set; }
+        public int cogQuant { get; set; }
+        public int gearQuant { get; set; }
+        public DateTime saleDate { get; set; }
+        public double salesTaxPercent { get; set; }
+        public double cogPrice { get; set; }
+        public double gearPrice { get; set; }
+
+        public Reciept()
+        {
+            this.customerID = 0;
+            this.cogQuant = 0;
+            this.gearQuant = 0;
+            this.saleDate = DateTime.Now;
+            this.salesTaxPercent = 0.089;
+            this.gearPrice = 250.00;
+            this.cogPrice = 79.99;
+        }
+        public Reciept(int id, int cog, int gear)
+        {
+            this.customerID = id;
+            this.cogQuant = cog;
+            this.gearQuant = gear;
+            this.saleDate = DateTime.Now;
+            this.salesTaxPercent = 0.089;
+            this.gearPrice = 250.00;
+            this.cogPrice = 79.99;
+        }
+        public double CalculateTotal()
+        {
+            double netAmount = this.CalculateNetAmount();
+            double salesTaxAmount = this.CalculateTaxedAmount();
+            double totalAmount = netAmount + salesTaxAmount;
+            return totalAmount;
+        }
+        public void PrintReciept()
+        {
+            Console.WriteLine("=======================");
+            Console.WriteLine($"Customer ID: {this.customerID}");
+            Console.WriteLine($"# of cogs: {this.cogQuant}");
+            Console.WriteLine($"# of gears: {this.gearQuant}");
+            Console.WriteLine($"Net Amount: {CalculateNetAmount():C2}");
+            Console.WriteLine($"Sales Tax: {CalculateTaxedAmount():C2}");
+            Console.WriteLine($"Grand Total: {CalculateTotal():C2}");
+            Console.WriteLine($"Time: {this.saleDate}");
+            Console.WriteLine("=======================");
+        }
+        private double CalculateTaxedAmount()
+        {
+            double netAmount = this.CalculateNetAmount();
+            double salesTax = netAmount * this.salesTaxPercent;
+            return salesTax;
+        }
+        private double CalculateNetAmount()
+        {
+            double markup = 0.15;
+            if(this.cogQuant > 10 || this.gearQuant>10 || this.cogQuant+this.gearQuant>16)
+            {
+                markup = .125;
+            } else
+            {
+                markup = .15;
+            }
+
+            double netAmount = this.cogQuant * this.cogPrice * (1 + markup) + this.gearQuant * this.gearPrice * (1 + markup);
+
+            return netAmount;
+        } 
+    }
+}
